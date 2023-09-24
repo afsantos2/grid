f9d1aa001e89da6154335ca5acda63109ea5375c
afsantos2 <anderson_f.ads@hotmail.com>
2023-09-24 01:29:40 -0300

    WIP: Exibe somente o header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import Produtos from './components/Produtos';
+import Header from './components/Header';
 import './App.css';
+
+// import Produtos from './components/Produtos';
+// import Paginacao from './components/Paginacao';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <main>
-      <ul className="produtos-lista">
-        <Produtos />
-      </ul>
+      {/* 
+        <ul className="produtos-lista">
+          <Produtos />         
+        </ul> 
+      */}
       </main>
     </div>
   );
