c92163261256a2afce05f966cf56c980dd098449
afsantos2 <anderson_f.ads@hotmail.com>
2023-09-24 23:19:40 -0300

    Implementa busca de animes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
 import Header from './components/Header';
+import SearchInput from './components/SearchInput';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-// import Produtos from './components/Produtos';
-// import Paginacao from './components/Paginacao';
+const api = 'https://kitsu.io/api/edge/'
 
 function App() {
+  const [info, setInfo] = useState({})
+  const [text, setText] = useState('')
+  
+  useEffect(() => {
+    setInfo({})
+    if(text) {
+      fetch(`${api}anime?filter[text]=${text}&page[limit]=16`)
+        .then((response) => response.json())
+        .then((response) => {
+          setInfo(response)
+        })
+    }  
+  }, [text])
+
   return (
     <div className="App">
       <Header />
       <main>
-      {/* 
-        <ul className="produtos-lista">
-          <Produtos />         
-        </ul> 
-      */}
+        <SearchInput 
+          value={text}
+          onChange={(search) => setText(search)}
+        />
+
+        {/* loadind button */}
+        {text && !info.data && (
+          <div className="h-12 border-4 border-solid rounded-full loading-w-12 border-r-black animate-spin"></div>
+        )}
+        
+        {info.data && (
+          <ul className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center'>
+            {info.data.map((anime) => (
+              <li key={anime.id} className='flex flex-col items-center'>
+                <img
+                  src={anime.attributes.posterImage.small}
+                  alt={anime.attributes.canonicalTitle}                  
+                />
+                {anime.attributes.canonicalTitle}
+              </li>
+            ))}
+          </ul>
+        )}
       </main>
     </div>
   );
