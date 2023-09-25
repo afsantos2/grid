e2cd5a17176bf7fd1a349ec12428291fcf578348
afsantos2 <anderson_f.ads@hotmail.com>
2023-09-24 22:20:46 -0300

    Implementa debounce

---
+++ src/useDebounce.js
@@ -0,0 +1,17 @@
+import { useRef } from "react"
+
+export default function useDebounce(fn, delay) {
+  const timeoutRef = useRef(null)
+
+  console.log(timeoutRef)
+
+  function debouncedFn(...args) {
+    clearTimeout(timeoutRef.current)
+
+    timeoutRef.current = setTimeout(() => {
+      fn(...args)      
+    }, delay)
+  }
+
+  return debouncedFn;
+}
