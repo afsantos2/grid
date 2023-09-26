9b3364ceae5b18d0d6898155909f4f990459fcd0
afsantos2 <anderson_f.ads@hotmail.com>
2023-09-25 21:34:26 -0300

    Remove componentes desnecessários

--- src/components/Data.js
+++
@@ -1,5 +0,0 @@
-export const produtos = [
-  {id: 1, img_src: '/' ,nome: 'Mouse Bluetooth', preco: 'R$99,00' },
-  {id: 2, img_src: '/' ,nome: 'Teclado Mecânico', preco: 'R$50,00' },
-  {id: 3, img_src: '/' ,nome: 'Fone Bluetooth', preco: 'R$100,00'},
-];
